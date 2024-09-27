42df71d6adf6fe2b17c805f3716f5eb0b4dc759b
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2024-09-27 15:37:32 +0200

    Tested home screen

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { getByRole, render, screen, act, getByText } from '@testing-library/react';
 import App from '../App';
+import { getAll } from '../models/fetch';
+
+jest.mock("../models/fetch", () => ({
+    getAll: jest.fn(),
+}));
 
 describe('Tests main app functionality', ()  => {
     beforeEach(() => {
-        jest.unmock('../models/fetch');
+        jest.clearAllMocks();
     });
 
     test("renders and displays start screen, header and footer", async () => {
-
         render(<App url="/" />);
         await screen.findByText('Home'); // Change here if header is changed.
         expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Home');
@@ -16,4 +20,20 @@ describe('Tests main app functionality', ()  => {
         expect(footerElement).toBeInTheDocument();
         expect(footerElement).toHaveTextContent('Challe_P and narwhal');
     });
+
+    test("should render documents on start page", async () => {
+        const mockResponse = { data: [
+            { title: "Fake title", content: "Fake content"}, 
+            { title: "A title", content: "Some content" }]};
+        getAll.mockResolvedValue(mockResponse);
+        await act(async () => {
+            await render(<App />);
+        })
+        await screen.findByText('Home'); // Change here if header is changed.
+        const links = screen.getAllByRole('link');
+        const linkFake = links.some(link => link.textContent === "Fake title");
+        const otherLink = links.some(link => link.textContent === "A title");
+        expect(linkFake).toBe(true);
+        expect(otherLink).toBe(true);        
+    })
 });
