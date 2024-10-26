1399bfa186cbadb1b85de10109f2082c804dadc5
Alf Pettersson <alpt22@student.bth.se>
2024-10-26 12:13:23 +0200

    Updated update-form and utils without referencing api keys

---
+++ src/forms/update-form.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+import { updateDocument, getOne, removeOne } from "../models/fetch";
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from 'react-router-dom';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { baseURL, frontURL } from '../utils';
+import Mailgun from 'mailgun.js';
+
+// Mailgun
+const formData = require('form-data');
+// const Mailgun = require('mailgun.js');
+
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({username: 'api', key: "YOUR_KEY_HERE"});
+
+export function UpdateForm() {
+    // Den här hämtar datan hela tiden. Borde hämta en gång och sen låta det vara? Iof när socketen ska igång är det ju bra.
+    const { id } = useParams();
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const navigate = useNavigate();
+
+    const location = useLocation();
+    const message = location.state ? location.state.message : "";
+    
+    const {
+        register,
+        handleSubmit,
+        watch,
+        setValue,
+        control,
+        formState: { errors },
+    } = useForm();
+    
+    // Fetch document and set field values to document title and content
+    useEffect (() => {
+        const fetchData = async () => {
+            try {
+                const doc = await getOne(id);
+
+                //Change state
+                setContent(doc.content);
+                setTitle(doc.title);
+
+                //Change input field values
+                setValue("title", doc.title);
+                setValue("content", doc.content); 
+
+            } catch (error) {
+                console.error(error);
+                navigate("/", { state: { message: "An error occurred"}});
+            }
+        };
+
+        fetchData();
+    }, [id, setValue]);
+
+    const onSubmit = async (data) => {
+        data.content = content;
+        if (data.title === "") {
+            data.title = title;
+        };
+
+        const response = await updateDocument(data);
+
+        if (response.status === 200) {
+            navigate("/", { state: { message: "Document was successfully updated."}});
+        } else {
+            console.log("Server issues.");
+            console.error(response);
+        }
+
+    };
+
+    const handleDelete = async () => {
+        try {
+            const response = await removeOne(id);
+            
+            if (response.status === 200) {
+                navigate("/", { state: { message: "Document was successfully removed."}});
+            } else {
+                console.log("Server issues.");
+                console.error(response);
+            }
+        } catch (error) {
+            console.log("Something went wrong");
+            console.error(error);
+        }
+    };
+
+    const handleShare = async (data) => {
+        console.log("Share this document.", data);
+        
+        // await mg.messages.create(mailgunUtils.domain, {
+        //     from: `Excited User <mailgun@${mailgunUtils.domain}>`,
+        //     to: [data.email],
+        //     subject: "Hello from Challe P and Narwhal! --('' c ){",
+        //     text: "Sign up at Challe P and Narwhal!",
+        //     html: `
+        //     <h1>Sign up at Challe P and Narwhal!</h1>
+        //     <a href="${frontURL}/register">Register here</a>
+        //     `
+        // })
+        // .then(msg => console.log(msg)) // logs response data
+        // .catch(err => console.error(err)); // logs any error
+        navigate("#", { state: { message: `Document is now shared with ${data.email}!`}});
+
+    }
+
+    return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <>
+        <h1>{message}</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="title">Title</label>
+            <input id="title" type="text" defaultValue={title} {...register('title')} />
+            <ReactQuill ReactQuill theme="snow" id='content' value={content} onChange={setContent} />
+            <input type="text" name="id" hidden defaultValue={id} {...register("id")}/>
+            <input type="submit" value="Save changes" />
+            <button type="button" onClick={handleDelete}>Delete document</button>
+        </form>
+        <form onSubmit={handleSubmit(handleShare)}>
+            <input type="email" id="email" {...register('email')} />
+            <input type="submit" value="Share document" />
+        </form>
+    </>
+    );
+}
---
+++ src/utils.js
@@ -0,0 +1,4 @@
+const baseURL = process.env.NODE_ENV === 'development' ? "http://localhost:1338" : "https://jsramverk-backend-vima23.azurewebsites.net";
+const frontURL = process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://www.student.bth.se/~alpt22/editor/";
+
+export { baseURL, frontURL };
